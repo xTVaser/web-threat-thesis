327251c0d06d70d447babbb9edc4fa9461b7a6ee
Tyler Wilding <xtvaser@gmail.com>
2016-12-29 16:10:44 -0500

    Changed folder structure of parser, begin work

--- Parser/generateTest.py
+++
@@ -1,8 +0,0 @@
-# Seperate our sample files into files with for example 500 sql, another 500 xss, etc. 250 normal requests.
-# Use this file to combine them all into one test file that we can use.
-
-# usage would be generateTest.py -xss <file with xss> -normal <file with non-attacks>...so on...-xssPer 25 -sqlPer 25,
-# allows us to change the proportion of the test files as well for later on if that is needed.
-
-# <request> <type> type is 0-4, 0 being a non threat, 1 being sql, 2 xss, 3 rfi attack.
-
