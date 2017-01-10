3fa0cca6c888cf449d7c2b75643afaf2524fef1e
Tyler Wilding <xtvaser@gmail.com>
2017-01-10 16:33:18 -0500

    Created non-threat testing data

--- Backup Data/Request Scripts/cleanNonThreats.py
+++ Backup Data/Request Scripts/cleanNonThreats.py
@@ -2,20 +2,28 @@ import os, random
 
 os.chdir(os.getcwd())
 
-file = open("nonthreats")
+file = open("NT_Training")
 
-newFile = open("nonthreats_new", "w+")
+newFile = open("NT_Training_New", "w+")
 
 lines = []
 
 for line in file:
-    contents = line.split(" ")
-    if len(contents) is 3:
-        newLine = contents[1].replace("/fab/memelords/", "/test/website/")
-        lines.append(newLine + "\n")
+    contents = line.split()
+
+    requestLine = contents[len(contents)-1]
+
+    newRequestLine = "/test/website"
+    urlParams = requestLine.split("/")
+
+    for x in range(len(urlParams)-3):
+        newRequestLine += "/" + urlParams[x+3]
+
+    lines.append(newRequestLine)
+
 
 distinctLines = list(set(lines))
 random.shuffle(distinctLines)
 
 for line in distinctLines:
-    newFile.write(line)
+    newFile.write(line + "\n")
